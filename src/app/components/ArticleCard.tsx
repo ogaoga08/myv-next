1705378f837e062d4e1ed021c6d5301e0f5f2e7f
ogaoga08 <kamekameshin0514@gmail.com>
2024-10-24 18:17:36 +0900

    DBのキーをcreatedAtに変更(仮)

--- src/app/components/ArticleCard.tsx
+++ src/app/components/ArticleCard.tsx
@@ -18,11 +18,11 @@ const ArticleCard = ({ article }: ArticleCardProps) => {
   }
 
   return (
-    <article className="shadow my-4 flex flex-col" key={article.id}>
+    <article className="shadow my-4 flex flex-col" key={article.createdAt}>
       <div className="bg-white flex flex-col justify-start p-6 rounded-md">
         <p className="text-sm pb-3 text-sky-800">/{article.id}</p>
         <Link
-          href={`/articles/${article.id}`}
+          href={`/articles/${article.createdAt}`}
           className="text-slate-900 text-2xl font-bold hover:text-gray-700 pb-1"
         >
           {article.title}
@@ -30,7 +30,10 @@ const ArticleCard = ({ article }: ArticleCardProps) => {
         <p className="text-sm pb-3 text-slate-900">
           {new Date(article.createdAt).toLocaleString()}
         </p>
-        <Link href={`/articles/${article.id}`} className="text-slate-900 pb-4">
+        <Link
+          href={`/articles/${article.createdAt}`}
+          className="text-slate-900 pb-4"
+        >
           {article.content.length > 70
             ? article.content.substring(0, 70) + "..."
             : article.content}
--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -12,7 +12,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
       {articles.map((article) => (
-        <ArticleCard article={article} key={article.id}></ArticleCard>
+        <ArticleCard article={article} key={article.createdAt}></ArticleCard>
       ))}
     </div>
   );
