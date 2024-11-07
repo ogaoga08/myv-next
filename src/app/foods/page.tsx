83615bd0a8ac9a7784e52b5575fd080f11bf28d4
ogaoga08 <kamekameshin0514@gmail.com>
2024-11-08 00:01:21 +0900

    レイアウト修正

--- src/app/foods/page.tsx
+++ src/app/foods/page.tsx
@@ -13,9 +13,6 @@ export default async function Page({
 
   return (
     <div className="w-full px-10">
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl">Food Menu</h1>
-      </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search foods..." />
       </div>
