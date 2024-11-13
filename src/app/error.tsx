1dccd1d55553e4cb45357892a11ed6616e6d9196
ogaoga08 <kamekameshin0514@gmail.com>
2024-11-13 14:11:28 +0900

    エラーページのボタンを戻るに変更

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const Error = ({ reset }: { reset: () => void }) => {
+  const router = useRouter();
   return (
     <div className="bg-red-100 border-l-4 border-red-500 text-red-700 mt-4 rounded shadow-md mx-auto p-2">
       <h3 className="font-bold mb-2">エラーが発生しました。</h3>
       <button
-        onClick={() => reset}
+        onClick={() => router.back()}
         className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-500 transition duration-200"
       >
-        もう一度試す
+        戻る
       </button>
     </div>
   );
