f41fe5c56a57c82b3e21d6135bee066b94f2f9ed
ogaoga08 <kamekameshin0514@gmail.com>
2024-11-18 23:08:26 +0900

    検索時のクエリパラメータ変更

--- src/app/foods/page.tsx
+++ src/app/foods/page.tsx
@@ -6,7 +6,6 @@ export default async function Page({
 }: {
   searchParams?: {
     query?: string;
-    page?: string;
   };
 }) {
   const query = searchParams?.query || "";
@@ -14,7 +13,7 @@ export default async function Page({
   return (
     <div className="w-full px-10">
       <div className="mt-4 flex items-center justify-between md:mt-8">
-        <Search placeholder="部位名で検索..." />
+        <Search placeholder="部位名・キーワードを入力" />
       </div>
       <PartsTable query={query} />
     </div>
