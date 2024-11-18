ad4b5ddeccfd4863879b3b9c75cffe205abc9517
ogaoga08 <kamekameshin0514@gmail.com>
2024-11-19 00:42:47 +0900

    iPhoneで検索時にフォーカスしないようにした

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+      ></meta>
       <body className=" bg-slate-200 text-slate-50">
         <div className="flex flex-col min-h-screen">
           <Header />
