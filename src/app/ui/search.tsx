bfe9d68592148fc9736bb886abe92b425e223db3
ogaoga08 <kamekameshin0514@gmail.com>
2024-11-27 01:30:24 +0900

    safariの検索中にスクロール位置固定！

--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -3,22 +3,28 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
+import { useEffect, useRef } from "react";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+  const scrollPosition = useRef(0);
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams || "");
-    // params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
+    scrollPosition.current = window.scrollY; // 現在のスクロール位置を保存
     replace(`${pathname}?${params.toString()}`);
   }, 300);
+
+  useEffect(() => {
+    window.scrollTo(0, scrollPosition.current); // スクロール位置を復元
+  }, [searchParams]);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0 m-0">
