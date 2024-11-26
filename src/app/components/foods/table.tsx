022d7c5d0ccb76fcbcedc1c5ae5a75fab76a56b0
ogaoga08 <kamekameshin0514@gmail.com>
2024-11-27 00:45:05 +0900

    検索サジェスト欄を他要素の上に表示

--- src/app/components/foods/table.tsx
+++ src/app/components/foods/table.tsx
@@ -10,21 +10,25 @@ export default async function PartsTable({ query }: { query: string }) {
   }
 
   return (
-    <div className="flex-1 rounded-lg bg-gray-50 p-2 shadow-md overflow-y-auto">
-      {parts?.map((part) => (
-        <Link
-          href={part.route}
-          key={part.engname}
-          className=" w-full bg-gray-50 p-3 flex items-center justify-between hover:bg-gray-200 rounded-md"
-        >
-          <div>
-            <div className="flex items-center text-sm">
-              <p className="font-bold text-gray-900">{part.name}</p>
-              <p className="font text-sm text-gray-500 mx-2">{part.engname}</p>
+    <div className="relative flex flex-1 m-0">
+      <div className="absolute w-full rounded-lg bg-gray-50 p-2 shadow-md z-10 max-h-64 overflow-y-auto">
+        {parts?.map((part) => (
+          <Link
+            href={part.route}
+            key={part.engname}
+            className="w-full bg-gray-50 p-3 flex items-center justify-between hover:bg-gray-200 rounded-md"
+          >
+            <div>
+              <div className="flex items-center text-sm">
+                <p className="font-bold text-gray-900">{part.name}</p>
+                <p className="font text-sm text-gray-500 mx-2">
+                  {part.engname}
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
