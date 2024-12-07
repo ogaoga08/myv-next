a6b249b5fceffa362c7c36f07649d2b0e2f7fb21
ogaoga08 <kamekameshin0514@gmail.com>
2024-12-08 00:00:39 +0900

    auth.tsにgoogle

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const config: NextAuthConfig = {
   theme: {
@@ -9,6 +10,10 @@ export const config: NextAuthConfig = {
     Github({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   basePath: "/api/auth",
